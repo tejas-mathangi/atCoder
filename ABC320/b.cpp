ed9328f39108fe47dcf6f66922ec6fd23c06fec9
Tejas Mathangi <mathangi.tejas@gmail.com>
2026-05-20 03:40:05 +0530

    B - Longest Palindrome

--- ABC320/b.cpp
+++ ABC320/b.cpp
@@ -1,11 +1,38 @@
 // https://atcoder.jp/contests/abc320/tasks/abc320_b
+// https://www.geeksforgeeks.org/dsa/longest-palindromic-substring/
 
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-int main(){
-    int a, b;
-    cin >> a >> b;
-    cout << (int)(pow(a, b) + pow(b, a)) << endl;
+int getLongestPal(string &s) {
+    
+    int n = s.length();
+    int start = 0, maxLen = 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= 1; j++) {
+            int low = i;
+            int high = i + j; 
+
+            while (low >= 0 && high < n && s[low] == s[high]) 
+            {
+                int currLen = high - low + 1;
+                if (currLen > maxLen) {
+                    start = low;
+                    maxLen = currLen;
+                }
+                low--;
+                high++;
+            }
+        }
+    }
+
+    return maxLen;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << getLongestPal(s) << endl;
+    return 0;
 }
